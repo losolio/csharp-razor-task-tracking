63e148c650daaf334b5398d2d886c606a67a369c
Ole Kristian Losvik <ole@losol.no>
2020-05-24 09:37:44 +0200

    t5

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -1,11 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+        private ApplicationDbContext _context;
+        public IEnumerable<Task> Tasks { get; set; }
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            Tasks = _context.Tasks
+                .OrderBy(x => x.Priority);
 
         }
     }
