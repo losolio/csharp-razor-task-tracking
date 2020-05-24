e7f33261a246636eb5185070271038a9243a5b64
Ole Kristian Losvik <ole@losol.no>
2020-05-24 09:22:48 +0200

    t3

--- Tasker/Pages/CreateTask.chstml.cs
+++ Tasker/Pages/CreateTask.chstml.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,17 @@ namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        [BindProperty]
+        public Task NewTask { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
